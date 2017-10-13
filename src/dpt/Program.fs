583e58f77e364de70001b535a571cd263230340a
kjnilsson <knilsson@pivotal.io>
2017-10-13 10:49:04 +0100

    support -u and -e + add RIDs

--- src/dpt/Program.fs
+++ src/dpt/Program.fs
@@ -3,13 +3,16 @@
 open System
 open Argu
 open RabbitMQ.Client
+open RabbitMQ.PerfTest.Metrics
 
 type Args =
-    | [<AltCommandLine("-x")>] Producers of count : int
-    | [<AltCommandLine("-y")>] Consumers of count : int
-    | [<AltCommandLine("-h")>] Uri of uri : string
-    | [<AltCommandLine("-s")>] Size of size : int
+    | [<AltCommandLine("-x")>] Producers of arg : int
+    | [<AltCommandLine("-y")>] Consumers of arg : int
+    | [<AltCommandLine("-h")>] Uri of arg : string
+    | [<AltCommandLine("-s")>] Size of arg : int
     | [<AltCommandLine("-a")>] Autoack
+    | [<AltCommandLine("-e")>] Exchange of arg : string
+    | [<AltCommandLine("-u")>] Queue of arg : string
 
 with
     interface IArgParserTemplate with
@@ -20,9 +23,12 @@ with
             | Uri _ -> "connection URI"
             | Size _ -> "message size in bytes"
             | Autoack _ -> "auto ack"
+            | Exchange _ -> "exchange name"
+            | Queue _ -> "queue name"
 
-let parser = ArgumentParser.Create<Args>(programName = "pf")
-
+let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
+let parser = ArgumentParser.Create<Args>(programName = "dpt",
+                                         errorHandler = errorHandler)
 
 let makeBodyGen size =
     let rem = size - 12
@@ -39,7 +45,6 @@ let makeBodyGen size =
             Array.Copy (s, 0, fill, 0, s.Length)
             Array.Copy (ts, 0, fill, 4, ts.Length)
             fill
-open RabbitMQ.PerfTest.Metrics
 
 type Scenario =
     { Uri : Uri
@@ -48,7 +53,9 @@ type Scenario =
       MsgBodyGen : int -> byte array
       AutoAck : bool
       Stats : Stats
-       }
+      Exchange : string
+      Queue : string option
+    }
     with
     static member def () =
             let id = sprintf "test-%s" (DateTime.Now.ToString("yyMMdd-hh:mm:ss"))
@@ -58,6 +65,8 @@ type Scenario =
               MsgBodyGen = makeBodyGen 0
               AutoAck = false
               Stats = Stats (id, 1000)
+              Exchange = "direct"
+              Queue = None
     }
     static member parse (args : Args list) =
         args
@@ -73,7 +82,10 @@ type Scenario =
                 {s with MsgBodyGen = makeBodyGen size}
             | Autoack ->
                 {s with AutoAck = true}
-
+            | Queue q ->
+                {s with Queue = Some q}
+            | Exchange e ->
+                {s with Exchange = e}
         ) (Scenario.def())
 
 let consume (s: Scenario) (m: IModel) queue f =
@@ -92,19 +104,24 @@ let consume (s: Scenario) (m: IModel) queue f =
             member __.Dispose () =
                 m.BasicCancel(consumerTag) }
 
-let declExchange (m: IModel) =
+let declExchange (m: IModel) { Exchange = exchange }  =
     let args = Collections.Generic.Dictionary ()
-    m.ExchangeDeclare ("direct", "direct", false, false, args)
+    m.ExchangeDeclare (exchange, "direct", false, false, args)
 
-let prepareQueue (m: IModel) exchange key =
-    let ok = m.QueueDeclare (exclusive = false)
+let prepareQueue (m: IModel) { Exchange = exchange; Queue = queue } key =
+    let ok =
+        match queue with
+        | Some q ->
+            m.QueueDeclare (queue = q, exclusive = false)
+        | None ->
+            m.QueueDeclare (exclusive = false)
     m.QueueBind (ok.QueueName, exchange, key)
     ok.QueueName
 
 let startConsumer (s: Scenario) (cf: ConnectionFactory) key =
     let c = cf.CreateConnection()
     let m = c.CreateModel()
-    let q = prepareQueue m "direct" key
+    let q = prepareQueue m s key
     consume s m q (fun tag _ body ->
         let recvTicks = DateTime.UtcNow.Ticks
         let sentTicks = BitConverter.ToInt64(body, 4)
@@ -119,7 +136,7 @@ let startProducer (s : Scenario) (cf: ConnectionFactory) key bodyGen =
     let stats = s.Stats
     async {
         while true do
-            m.BasicPublish ("direct", key, bp, bodyGen seqNum)
+            m.BasicPublish (s.Exchange, key, bp, bodyGen seqNum)
             stats.HandleSend ()
             seqNum <- seqNum + 1 }
     |> Async.Start
@@ -130,7 +147,7 @@ let run (s : Scenario) =
     cf.Uri <- s.Uri
     use c = cf.CreateConnection()
     use m = c.CreateModel()
-    declExchange m
+    declExchange m s
     let key = Guid.NewGuid().ToString()
     // first start consumers
     let consumers =
@@ -138,7 +155,7 @@ let run (s : Scenario) =
             yield startConsumer s cf key ]
     // if there are no consumers still create a queue
     if s.Consumers = 0 then
-        prepareQueue m "direct" key |> ignore
+        prepareQueue m s key |> ignore
     // then the producers
     let producers =
         [ for _ in [1 .. s.Producers] do
